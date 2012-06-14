d474dc5044d3584b04abc0f16b2a4a15d9933365
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2012-06-14 09:48:12 -0430

    Moved iute.core to https://github.com/plone-ve/iute.core

--- src/iute.core/iute/core/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from misc import *
-import install
-import has
-from iute.core.remoteservice import RemoteService
-from iute.core.browser.methodview import MethodView
-from iute.core.parsing import PrimitiveParser
