078f95f794a89333c1be9fa67476f8272db4e0bb
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2012-06-14 09:46:34 -0430

    Moved iute.thememain to https://github.com/plone-ve/iute.thememain

--- src/iute.thememain/README.txt
+++
@@ -1,8 +0,0 @@
-Introduction
-============
-
-A fork package from uwosh.thememain for IUTE Website
-
-An installable Plone theme used by the main Plone IUTE site that extends iute.themebase.
-
- 
--- src/iute.thememain/iute/thememain/interfaces.py
+++
@@ -1,7 +0,0 @@
-from zope.interface import Interface
-
-class ISectionNavigation(Interface):
-    """
-    A Folder can implement this to specify that it is a section navigation
-    folder
-    """
