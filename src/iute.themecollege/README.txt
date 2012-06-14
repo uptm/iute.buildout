a55f9163938dc53317b1d731a9beb091566c7621
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2012-06-14 09:47:19 -0430

    Moved iute.themecollege to https://github.com/plone-ve/iute.themecollege

--- src/iute.themecollege/README.txt
+++
@@ -1,4 +0,0 @@
-Introduction
-============
-
-This is a theme package that extends the iute.themebase package to provide a Plone theme suitable for Colleges.
--- src/iute.themecollege/iute/themecollege/__init__.py
+++
@@ -1,8 +0,0 @@
-dependencies = [
-    'iute.themebase',
-    'iute.default'
-]
-
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
